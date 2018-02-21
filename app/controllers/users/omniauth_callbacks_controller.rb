db9d183e64b34f96d29b4bd28e15ec83212f3f28
YauheniShakau <zhshakov@gmail.com>
2018-02-21 14:19:54 +0300

    bug fix

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,8 +3,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def self.provides_callback_for(provider)
     class_eval %Q{
       def #{provider}
+        puts 11
+        puts request.env["omniauth.auth"]
         @user = User.from_omniauth(request.env["omniauth.auth"])
-
+        puts 111
+        puts @user.valid?
+        puts @user.errors.inspect
         if @user.persisted?
           sign_in_and_redirect @user, event: :authentication
           set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   :validatable, :confirmable, :omniauthable,
   :omniauth_providers => [:facebook, :twitter, :vkontakte]
   has_many :comments
+  has_many :posts
 
   def self.from_omniauth(auth)
   	where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
