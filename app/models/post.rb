b348a66731210b3a11ffa802e07021cfd72f9438
YauheniShakau <zhshakov@gmail.com>
2018-02-26 16:11:41 +0300

    save

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
-  validates_presence_of :title, :description, :body
+  validates_presence_of :title, :description, :body, :image_url
   ratyrate_rateable "Rating"
   scope :with_ratings, ->{includes(:rate_average_without_dimension).order("rating_caches.avg desc")}
   has_one :rate_average_without_dimension, -> { where dimension: nil}, :as => :cacheable, :class_name => "RatingCache", :dependent => :destroy
